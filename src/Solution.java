4bdd6c89c62abd6bbb027a1ff649bc602d43bd81
Arvis Abromovics <info@vsmedio.lv>
2018-01-07 20:11:09 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+Переверни массив
+1. Создать массив на 10 чисел.
+2. Ввести с клавиатуры 10 чисел и записать их в массив.
+3. Расположить элементы массива в обратном порядке.
+4. Вывести результат на экран, каждое значение выводить с новой строки.
+
+
+Требования:
+1. Программа должна создавать массив на 10 целых чисел.
+2. Программа должна считывать числа для массива с клавиатуры.
+3. Программа должна выводить 10 строчек, каждую с новой строки.
+4. Массив должен быть выведен на экран в обратном порядке.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.valueOf(reader.readLine());
+        }
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            int tmp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = tmp;
+        }
+
+        for (int ar : arr) {
+            System.out.println(ar);
+        }
+    }
+}
